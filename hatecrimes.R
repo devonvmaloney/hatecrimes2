5e52e1730a579a7f889c1ce7a7dd0d88ba32f5ba
devonvmaloney <60240784+devonvmaloney@users.noreply.github.com>
2020-01-30 12:11:36 -0500

    Create hatecrimes.R
    Add R file

---
+++ hatecrimes.R
@@ -0,0 +1,118 @@
+# HATE CRIMES -- Group Project
+# Devon Maloney 
+# 1/29/2020
+
+# To set up the data, I seperated the national individual records of data and place into counts of incidents per month in excell by using the COUNTIF function for all unique month + year combinations. 
+# I did this again for just the New Jersery data, a state with a more complete record of hate crime data collection. 
+
+#### Packages and Directory ####
+install.packages("ggplot2") 
+install.packages("RColorBrewer")
+install.packages("ggridges")
+
+library(ggplot2)
+library(RColorBrewer) 
+library(ggridges)
+library(readr)
+
+setwd ("~/Documents/UNC/Spring 2020/456 Geovisualizing Change/HateCrime")
+
+hatedata <- read_csv("monthyear_indicident.csv")
+hatedataNJ <- read_csv("monthyear_indicidentNJ.csv")
+
+#### Bar graphs for seasonality ####
+#Make a bar graph 
+
+p = ggplot(hatedata, aes(x=Month, y = incident)) + geom_bar(stat="identity") 
+pnj = ggplot(hatedataNJ, aes(x=Month, y = incident)) + geom_bar(stat="identity") 
+
+#Add labels
+p = p + labs(title="Seaonality of hate crimes", x ="Month", y = "Number of indicdents", subtitle = "National rates", caption = "(based on data from the FBI)") 
+p # see graph 
+
+pnj = pnj + labs(title="Seaonality of hate crimes", x ="Month", y = "Number of indicdents", subtitle = "New Jersey rates", caption = "(based on data from the FBI)") 
+pnj # see graph 
+
+#### Warming Stripes  ####
+
+# National  
+# Colors 
+p = p + aes(fill = incident) # assign gradient of blue color to the temp value 
+p = ggplot(hatedata, aes(x=Monthyear, y = incident, fill = incident)) + geom_bar(stat="identity")
+display.brewer.all() # look at he brewer color library 
+p + scale_fill_gradientn(colours = colorRampPalette(brewer.pal(11,"RdYlBu"))(11)) # red to blue  
+p <- p + scale_fill_gradientn(colours = colorRampPalette(rev(brewer.pal(11,"RdYlBu")))(11)) # REV to adjust scale from blue to red 
+p
+p + aes(y = 300) # make it warming stripes
+p + aes(x = as.factor(Monthyear), y = 300) # to avoid missing stripes, make yearmonth a factor 
+
+# Add add minial theme, legend, titles, axis labels. 
+# Ideally, this section would also include a way to label the year on the x axis, but I was unable to do that. 
+p + aes(x = as.factor(Monthyear), y = 300) + theme_minimal() + theme(axis.text = element_blank(), axis.title = element_text(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='right') + labs (x= "Month", y = "Incidents", title = "National Incidents of Hate Crime", subtitle = "Monthly from 1991 - 2017", caption = "(based on data from the FBI)")
+
+# New Jersey 
+# Colors 
+pnj = pnj + aes(fill = incident) # assign gradient of blue color to the temp value 
+pnj = ggplot(hatedataNJ, aes(x=Monthyear, y = incident, fill = incident)) + geom_bar(stat="identity")
+display.brewer.all() # look at the brewer color library, select package 
+pnj + scale_fill_gradientn(colours = colorRampPalette(brewer.pal(11,"RdYlBu"))(11)) # red to blue  
+pnj <- pnj + scale_fill_gradientn(colours = colorRampPalette(rev(brewer.pal(11,"RdYlBu")))(11)) # REV to adjust scale from blue to red 
+pnj + aes(y = 300) # make it warming stripes
+pnj + aes(x = as.factor(Monthyear), y = 300) # to avoid missing stripes, make yearmonth a factor 
+
+# Add add minial theme, legend, titles, axis labels. 
+# Ideally, this section would also include a way to label the year on the x axis, but I was unable to do that. 
+pnj + aes(x = as.factor(Monthyear), y = 300) + theme_minimal() + theme(axis.text = element_blank(), axis.title = element_text(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='right') + labs (x= "Month", y = "Incidents", title = "New Jersey Incidents of Hate Crime", subtitle = "Monthly from 1991 - 2017", caption = "(based on data from the FBI)")
+
+#### PETAL CIRCLE ####
+# For this section, the independent variable is swtiched from Monthyear to Month
+
+# National  
+# Colors 
+p = p + aes(fill = incident) # assign gradient of blue color to the temp value 
+p = ggplot(hatedata, aes(x=Month, y = incident, fill = incident)) + geom_bar(stat="identity")
+display.brewer.all() # look at he brewer color library 
+p + scale_fill_gradientn(colours = colorRampPalette(brewer.pal(11,"RdYlBu"))(11)) # red to blue  
+p <- p + scale_fill_gradientn(colours = colorRampPalette(rev(brewer.pal(11,"RdYlBu")))(11)) # REV to adjust scale from blue to red 
+p
+p + aes(y = 300) # make it warming stripes
+p + aes(x = as.factor(Month), y = 300) # to avoid missing stripes, make yearmonth a factor 
+
+# Add add minimal theme, legend, titles, axis labels. 
+# Ideally, this section would also include a way to label the year on the x axis. 
+p + aes(x = as.factor(Month), y = 300) + theme_minimal() + theme(axis.text = element_blank(), axis.title = element_text(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='right') + labs (x= "Month", y = "Incidents", title = "National Incidents of Hate Crime", subtitle = "Monthly from 1991 - 2017", caption = "(based on data from the FBI)")
+
+# New Jersey 
+# Colors 
+pnj = pnj + aes(fill = incident) # assign gradient of blue color to the temp value 
+pnj = ggplot(hatedataNJ, aes(x=Month, y = incident, fill = incident)) + geom_bar(stat="identity")
+display.brewer.all() # look at he brewer color library 
+pnj + scale_fill_gradientn(colours = colorRampPalette(brewer.pal(11,"RdYlBu"))(11)) # red to blue  
+pnj <- pnj + scale_fill_gradientn(colours = colorRampPalette(rev(brewer.pal(11,"RdYlBu")))(11)) # REV to adjust scale from blue to red 
+pnj + aes(y = 300) # make it warming stripes
+pnj + aes(x = as.factor(Month), y = 300) # to avoid missing stripes, make month a factor 
+
+# Add add minimal theme, legend, titles, axis labels. Ideally, this section would also include a way to label the year on the x axis. 
+pnj + aes(x = as.factor(Month), y = 300) + theme_minimal() + theme(axis.text = element_blank(), axis.title = element_text(), panel.grid.major = element_blank(), panel.grid.minor = element_blank(), legend.position='right') + labs (x= "Month", y = "Incidents", title = "New Jersey Incidents of Hate Crime", subtitle = "Monthly from 1991 - 2017", caption = "(based on data from the FBI)")
+
+#Make it into a petal circle 
+p + coord_polar(theta = "x") 
+pnj + coord_polar(theta = "x") 
+
+#Tried to remove the unnessicary axises. Unknown why national graph is on 0.5 month increments and why the NJ is correctly on 1 month incriments 
+
+#### MONTHLY RIDGES ####
+# Tried to make this work but the program required the "missing aesthetics: y"
+
+p = ggplot(hatedata, aes(x = incident, y = Month, fill = ..x..)) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+  labs(title = 'National monthly rates of hate crimes') +
+  scale_fill_gradientn(colours = colorRampPalette(rev(brewer.pal(9,"RdYlBu")))(100))
+p 
+
+pnj = ggplot(hatedataNJ, aes(x = incident, y = Month, fill = ..x..)) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+  labs(title = 'New Jersey monthly rates of hate crimes ') +
+  scale_fill_gradientn(colours = colorRampPalette(rev(brewer.pal(9,"RdYlBu")))(100))
+pnj
+
